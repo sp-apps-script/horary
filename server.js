08445c200335ad70ae344793781e6995c8ec0f8d
Jean Cuadros <bikersprop@gmail.com>
2022-11-23 10:56:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
+// Config
+const sheetMainName = "horary";                     //Nombre de la hoja principal
+const sheetDaysName = "daysdefs";                   //Nombre de la hoja de los dias
+const sheetPeriodsName = "periods";                 //Nombre de la hoja de las horas
+const sheetClassesName = "classes";                 //Nombre de la hoja de las clases
+//const domain = '@vonex.edu.pe';
+const domain = '@';                                 //Validador de nombre de dominio del correo
+const className = `SM INT1 P LC (USMSI1022LCPA)`;   //Clase a buscar
+const daysColIndex = 0;                             //Indice de la columna Dia
+const emailColIndex = 7;                            //Indice de la columna Email
+const classColIndex = 10;                           //Indice de la columna Clase
+
 // Sheet
-const tableId = "1RWBItawrDM77Tb83fNokK0m6H6C_zJ6MwuvTjZ1LH1g";
-const ss = SpreadsheetApp.openById(tableId);
-const sheet = ss.getSheetByName("horary")
-const sheetDays = ss.getSheetByName("days")
-const sheetPeriods = ss.getSheetByName("periods")
+const googleSheetId = "1WnoXbqAPGNk3kwJ9L6JHQr0q31znZHCOIH0h03nVlrE";
+const ss = SpreadsheetApp.openById(googleSheetId);
+const sheetMain = ss.getSheetByName(sheetMainName)
+const sheetDays = ss.getSheetByName(sheetDaysName)
+const sheetPeriods = ss.getSheetByName(sheetPeriodsName)
+const sheetClasses = ss.getSheetByName(sheetClassesName)
 
 // Data
+let horaryClass = [];
 let horaryTeacher = [];
 let days = [];
 let hours = [];
-
-// Config
-const domain = '@';
-//const domain = '@vonex.edu.pe';
-let show = false;
+let hoursEnd = [];
+let show = true;
 
 //Get
 function doGet() {
@@ -22,34 +33,53 @@ function doGet() {
   let htmlOutput = HtmlService.createTemplateFromFile('index');
 
   // Read Sheet
-  let lastRow = sheet.getLastRow();
-  let lastCol = sheet.getLastColumn();
-  let horary = sheet.getRange(2, 1, lastRow, lastCol).getDisplayValues();
+  let lastRow = sheetMain.getLastRow();
+  let lastCol = sheetMain.getLastColumn();
+  let horary = sheetMain.getRange(2, 1, lastRow, lastCol).getDisplayValues();
+
+  // Get Days
+  let daysTemp = sheetDays.getRange("C2:C6").getDisplayValues();
+  daysTemp.forEach(day => days.push(day[daysColIndex]));
+
+  // Get Hours
+  let hoursLastRow = sheetPeriods.getLastRow();
+  let hoursTemp = sheetPeriods.getRange(2,4,(hoursLastRow - 1)).getDisplayValues();
+  let hoursEndTemp = sheetPeriods.getRange(2,5,(hoursLastRow - 1)).getDisplayValues();
+  hoursTemp.forEach(hour => hours.push(hour[0]));
+  hoursEndTemp.forEach(hour => hoursEnd.push(hour[0]));
+
+  // Validate Email Domain
+  email.includes(domain) ? show = true : show = false
 
   // Get Horary Teacher & Filter By Email
   horary.forEach((item) => {
-    if (item[7] == email) {
+    if (item[emailColIndex] == email) {
+      // Add Class Ext
+      for(let i = classColIndex; i < (classColIndex+3); i++){
+        if(item[i] != ""){
+          item[classColIndex] = `${item[classColIndex]}, ${item[i]}`;
+        }
+      }
+      // Add Email records
       horaryTeacher.push(item);
     }
   })
 
-  // Get Days
-  let daysTemp = sheetDays.getRange("C2:C6").getDisplayValues();
-  daysTemp.forEach(item => days.push(item[0]));
-
-  // Get Hours
-  let hoursTemp = sheetPeriods.getRange("B2:C8").getDisplayValues();
-  hoursTemp.forEach(item => hours.push(item[0]));
-
-  // Validate Email Domain
-  //console.log(`The word "${domain}" ${email.includes(domain) ? 'is' : 'is not'} in the sentence`);
-  email.includes(domain) ? show = true : show = false
+  // Get Horary Class & Filter By Class
+  horary.forEach((item) => {
+    if (item[classColIndex] == className) {
+      horaryClass.push(item);
+    }
+  })
 
   // Export Html
   htmlOutput.show = show;
   htmlOutput.email = email;
   htmlOutput.days = days;
   htmlOutput.hours = hours;
-  htmlOutput.dataHorary = horaryTeacher;
+  htmlOutput.hoursEnd = hoursEnd;
+  htmlOutput.cantHours = hoursLastRow - 1;
+  htmlOutput.horaryTeacher = horaryTeacher;
+  htmlOutput.horaryClass = horaryClass;
   return htmlOutput.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
